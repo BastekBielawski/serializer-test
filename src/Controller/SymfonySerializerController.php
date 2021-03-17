a8b3e41c14ab5074098bd11022f44e216e6f56df
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-03-17 21:40:19 +0100

    add test class

--- src/Controller/SymfonySerializerController.php
+++ src/Controller/SymfonySerializerController.php
@@ -6,15 +6,48 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Model\TestClass;
+use App\Model\TestValueObject;
 
 class SymfonySerializerController
 {
+    private Stopwatch $stopwatch;
+
+    private SerializerInterface $serializer;
+
+    /**
+     * @param Stopwatch           $stopwatch
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(Stopwatch $stopwatch, SerializerInterface $serializer)
+    {
+        $this->stopwatch = $stopwatch;
+        $this->serializer = $serializer;
+    }
+
     /**
      * @Route("symfony/serialize")
      */
     public function serializeAction(): Response
     {
-        return new JsonResponse();
+        $many = [];
+        for($i=0;$i<100;$i++) {
+            $many[] = new TestValueObject('dd'.$i);
+        };
+
+        $vo = new TestValueObject('wefwfwe');
+        $test = new TestClass($vo, $vo, $many);
+
+        $this->stopwatch->start('serialization');
+        for($i=0;$i<1000;$i++) {
+            $this->serializer->serialize($test, 'json');
+        }
+
+        $this->stopwatch->stop('serialization');
+
+        return new JsonResponse($this->stopwatch->getEvent('serialization')->getDuration());
     }
 
     /**
